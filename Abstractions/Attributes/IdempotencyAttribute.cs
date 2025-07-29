c4f74f143fa581c66467dcf2e25427b9e820721a
HachimEnnaji <ashennaji@hotmail.it>
2025-07-29 20:27:53 +0200

    - added redis servie

--- Abstractions/Attributes/IdempotencyAttribute.cs
+++ Abstractions/Attributes/IdempotencyAttribute.cs
@@ -4,54 +4,53 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Text.Json;
 
-namespace IdempotentApi.Abstractions.Attributes
+namespace IdempotentApi.Abstractions.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class IdempotencyAttribute(IIdempotencyServiceCache cache, int ttlMinutes = 60) : Attribute, IAsyncActionFilter
 {
-    [AttributeUsage(AttributeTargets.Method)]
-    public class IdempotencyAttribute(IIdempotencyServiceCache cache, int ttlMinutes = 60) : Attribute, IAsyncActionFilter
+    private readonly int _ttl = ttlMinutes;
+    private readonly IIdempotencyServiceCache _cache = cache
+        ?? throw new Exception("Missing IIdempotency Service");
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        private readonly int _ttl = ttlMinutes;
-        private readonly IIdempotencyServiceCache _cache = cache
-            ?? throw new Exception("Missing IIdempotency Service");
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        var key = context.HttpContext.Request.Headers["Idempotency-Key"].ToString();
+
+        if (string.IsNullOrWhiteSpace(key))
         {
-            var key = context.HttpContext.Request.Headers["Idempotency-Key"].ToString();
-
-            if (string.IsNullOrWhiteSpace(key))
-            {
-                context.Result = new BadRequestObjectResult("Idempotency-Key header is required.");
-                return;
-            }
-
-            //var cache = context.HttpContext.RequestServices.GetRequiredService<IIdempotencyService>();
-
-            var cached = await _cache.GetKeyAsync(key);
-
-            if (cached is not null)
-            {
-                var response = JsonSerializer.Deserialize<IdempotencyResponse>(cached);
-                context.Result = new ObjectResult(response)
-                {
-                    StatusCode = response?.IdemPotencyStatusCode ?? 200,
-                };
-                return;
-            }
-
-            var execution = await next();
-
-            if (execution.Result is ObjectResult objectResult)
-            {
-
-                var response = new IdempotencyResponse
-                {
-                    Result = objectResult.Value,
-                    IdemPotencyStatusCode = objectResult.StatusCode ?? 200,
-                };
-
-                var serialized = JsonSerializer.Serialize(response);
-
-                await _cache.SetKeyAsync(key, serialized, TimeSpan.FromSeconds(_ttl));
-            }
-
+            context.Result = new BadRequestObjectResult("Idempotency-Key header is required.");
+            return;
         }
+
+        //var cache = context.HttpContext.RequestServices.GetRequiredService<IIdempotencyService>();
+
+        var cached = await _cache.GetKeyAsync(key);
+
+        if (cached is not null)
+        {
+            var response = JsonSerializer.Deserialize<IdempotencyResponse>(cached);
+            context.Result = new ObjectResult(response)
+            {
+                StatusCode = response?.IdemPotencyStatusCode ?? 200,
+            };
+            return;
+        }
+
+        var execution = await next();
+
+        if (execution.Result is ObjectResult objectResult)
+        {
+
+            var response = new IdempotencyResponse
+            {
+                Result = objectResult.Value,
+                IdemPotencyStatusCode = objectResult.StatusCode ?? 200,
+            };
+
+            var serialized = JsonSerializer.Serialize(response);
+
+            await _cache.SetKeyAsync(key, serialized, TimeSpan.FromSeconds(_ttl));
+        }
+
     }
 }
---
+++ Abstractions/RedisCacheService.cs
@@ -0,0 +1,27 @@
+﻿using IdempotentApi.Abstractions.Interfaces;
+using StackExchange.Redis;
+
+namespace IdempotentApi.Abstractions;
+
+public class RedisCacheService(IConnectionMultiplexer redis) : IIdempotencyServiceCache
+{
+    private readonly IDatabase _redis = redis.GetDatabase()
+        ?? throw new ArgumentNullException(nameof(redis));
+
+    public async Task<bool> HasResponseAsync(string key)
+       => await _redis.KeyExistsAsync(key);
+    public async Task<string?> GetKeyAsync(string key)
+    {
+
+        var response = await _redis.StringGetAsync(key);
+
+        return response.HasValue ? response.ToString() : null;
+    }
+
+    public async Task SetKeyAsync(string key, string value, TimeSpan? expiration = null)
+    {
+        await _redis.StringSetAsync(key, value, expiration ?? TimeSpan.FromMinutes(5));
+    }
+
+
+}
